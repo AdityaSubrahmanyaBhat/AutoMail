b58e48cdf54b088a92caa134e8e7e69e5b4e1ca1
AdityaSubrahmanyaBhat <70971391+AdityaSubrahmanyaBhat@users.noreply.github.com>
2022-01-17 11:50:42 +0530

    removed email addresses

--- mail.py
+++ mail.py
@@ -2,9 +2,8 @@ import smtplib
 
 
 
-
 def send_mail(message):
     server=smtplib.SMTP('smtp.gmail.com',587)
     server.starttls()
-    server.login('4ni19cs010_a@nie.ac.in','password_goes_here')
-    server.sendmail('4ni19cs010_a@nie.ac.in','adityasbhat1234@gmail.com',message)
+    server.login('from address','password_goes_here')
+    server.sendmail('from address','to address',message)
