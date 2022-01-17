013895c2b227fb6294c334ff1ad2fefd12bf8cf9
AdityaSubrahmanyaBhat <70971391+AdityaSubrahmanyaBhat@users.noreply.github.com>
2022-01-17 11:45:37 +0530

    changed record duration

--- listen.py
+++ listen.py
@@ -4,13 +4,11 @@ import mail
 
 listener=sr.Recognizer()
 
-def get_audio():
+def get_audio(duration=5):
     with sr.Microphone() as source:
-        # audio=listener.listen(source)
-        audio=listener.record(source=source,duration=10)
+        audio=listener.record(source=source,duration=duration)
         final_audio=listener.recognize_google(audio)
         print(final_audio)
-        # listener.stop()
         return final_audio
 
 responses=["yes","yes please","yes Friday","yes I do","yes I want to send a mail","yes send it","yes Friday send it"]
@@ -21,7 +19,7 @@ def listen():
     audio=get_audio()
     if audio in responses:
         talk.talk("Ok sure , What is the message?")
-        message=get_audio()
+        message=get_audio(10)
         talk.talk("Shall i send it sir?")
         confirmation=get_audio()
         if confirmation in responses:
